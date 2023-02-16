ad4c89dcdfbfc5e6aa43d4f24a0e5d46d08dc052
juan-para <juanpara86@gmail.com>
2023-02-16 19:58:36 -0300

    Initial commit

---
+++ src/test/java/com/expensesexpenses/MonthlyExpensesControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensesexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlyExpensesControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
